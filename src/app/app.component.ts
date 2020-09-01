8fbd43308068ee1eb1f94775b0542e2c13845654
Teresita Cuccia <terecuccia@gmail.com>
2020-09-01 19:00:47 -0300

    Blog de Pipes con Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  nombre: string = 'Capitán América';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  personajes: string[] = ['Ironman', 'Spiderman', 'Black widow', 'Thor', 'Loki', 'Groot'];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+}
