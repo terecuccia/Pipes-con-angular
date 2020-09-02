55e36f84ce031a8c71f34359b37dfb11c221ad97
Teresita Cuccia <terecuccia@gmail.com>
2020-09-02 17:58:28 -0300

    Agregué pipe async y date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,13 @@ export class AppComponent {
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 1234.5;
+  fecha: Date = new Date();
+
+  valorPromesa = new Promise<string>( (resolve) => {
+    setTimeout(() => {
+      resolve('Llegó la data');
+    }, 4500);
+  });
 
   heroe = {
     nombre: 'Logan',
